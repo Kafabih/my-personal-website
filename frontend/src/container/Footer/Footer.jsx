907f1ea94951d7f3b637fdebcfc499a5af14dfdb
Kafabih <kafabih@net2software.com>
2022-04-28 08:00:33 +0700

    change in footer about email sec

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:abikafabih@gmail.com" className="p-text">
+          <a href="mailto:your email" className="p-text">
             Email Me
           </a>
         </div>
